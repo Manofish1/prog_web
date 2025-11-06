7f32fb8b8c1063bf4d967358aeec84ec6ee7ffa7
DAVI GONCALVES <davig3335@gmail.com>
2025-11-06 10:15:11 -0300

    Update salvar-funcionario2.php

--- projeto/salvar-funcionario2.php
+++ projeto/salvar-funcionario2.php
@@ -20,11 +20,31 @@
     		}
     		break;
 
-    	case 'editar':
-            //code...
-    	    break;
+			case 'editar':
+				$nome = $_POST['nome_funcionario'];
+				$email = $_POST['email_funcionario'];
+				$telefone = $_POST['telefone_funcionario'];
+	
+				$sql = "UPDATE funcionario SET  nome_funcionario='{$nome}', email_funcionario='{$email}', telefone_funcionario='{$telefone}' WHERE  id_funcionario=".$_REQUEST['id_funcionario'];
+	$res = $conn->query($sql);
+	if($res == true){
+		print "<script>alert('Editou com sucesso!');</script>";
+		print "<script>location.href='?page=listar-funcionario';</script>"; 	  
+	}else{
+		print "<script>alert('Não editou!');</script>";
+		print "<script>location.href='?page=listar-funcionario';</script>"; 	 
+	}
+		break;
 
     	case 'excluir':
-    		// code...
+    		$sql = "DELETE FROM  funcionario WHERE  id_funcionario=".$_REQUEST['id_funcionario'];
+			$res = $conn->query($sql);
+			if($res == true){
+				print "<script>alert('Excluiu com sucesso!');</script>";
+				print "<script>  location.href='?page=listar-funcionario';</script>"; 	  
+			}else{
+				print "<script>alert('Não excluiu!');</script>";
+				print "<script>  location.href='?page=listar-funcionario';</script>"; 	 
+			}
     		break;
-    }
+		}
